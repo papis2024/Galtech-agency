65a6273ed77a751bb2bf2fda673e564c79e09d46
papis2024 <167688794+papis2024@users.noreply.github.com>
2025-01-30 22:21:06 +0000

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,21 +1,7 @@
-from django import views
 from django.contrib import admin
 from django.urls import path,include
-from .views import *
 
 urlpatterns = [
-   
-    path('',accueil, name="accueil"),
-    path('login',login, name="login"),
-    path('register',register, name="register"),
-    path('home',home, name="home"),
-    path('contact',contacter, name="cl"),
-    path('apropos',apropos, name="apropos"),
-    path('gallerie',gallerie, name="gallerie"),
-    path('reserver',reserver, name="reserver"),
-    path('client',gestion, name="cli"),
-    path('delete/<int:id>/',supprimer_donner, name="supprimerdonner"),
-    path('<int:id>/',modifier_donner, name="modifierdonner"),
-   
-    
+    path('admin/', admin.site.urls),
+    path('', include('agence.urls')),
 ]
